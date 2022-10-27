e17b3cacb5252284a2a60807113fcdc485f17732
boluajileye <boluwatifeajileye@gmail.com>
2022-10-27 12:41:31 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json;");
+
+        $hng = [
+            "slackusername"=> "ajileyebolu",
+            "backend"=>TRUE,
+            "age"=> 23,
+            "bio"=> "I am Ajileye Boluwatife, A recent graduate transitioning into tech. I'm so glad to be part of HNG9 interns"
+        
+        ];
+    
+    echo json_encode($hng);
+
+?>
