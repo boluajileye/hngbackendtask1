ee778a9ece4fda06c9202ed8091a58beb55fe364
Ajileye Boluwatife <boluwatifeajileye@gmail.com>
2022-10-29 17:25:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json;");
 
         $hng = [
-            "slackusername"=> "ajileyebolu",
+            "slackUsername"=> "ajileyebolu",
             "backend"=>TRUE,
             "age"=> 23,
             "bio"=> "I am Ajileye Boluwatife, A recent graduate transitioning into tech. I'm so glad to be part of HNG9 interns"
